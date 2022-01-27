6ad0cef24f1110817f9908b4f2a6a8a876c1cfed
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-01-27 17:13:42 +0200

    routes now have own module

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
-from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 import re
-
-app = Flask(__name__)
+from app.__init__ import app
+from repositories.database import db
 
 @app.route("/")
 def index():
@@ -11,36 +10,49 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if (request.method == "POST"
-    and "username" in request.form
-    and "password" in request.form):
-        pass
+        and "username" in request.form
+        and "password" in request.form):
+        username = request.form["username"]
+        password = request.form["password"]
+        if db.user_validation(username, password):
+            return redirect("/")
+
     msg = "Wrong username or password"
-    return render_template("login.html", msg = msg)
+    return render_template("index.html", msg = msg)
 
 @app.route("/register", methods=["GET", "POST"])
 def create_acc():
-    if (request.method == "POST" 
-    and "username" in request.form 
-    and "email" in request.form 
-    and "password" in request.form 
-    and "repeat_pw" in request.form):
-        
+    if (request.method == "POST"
+        and "username" in request.form
+        and "email" in request.form
+        and "password" in request.form
+        and "repeat_pw" in request.form):
         username = request.form["username"]
         email = request.form["email"]
         password = request.form["password"]
         repeat_pw = request.form["repeat_pw"]
-
-        if password != repeat_pw:
-            pass
-        if len(username) < 6 or len(username) > 20:
-            pass
-        if len(password) < 8 or len(password) > 20:
-            pass
-        if username:
-            pass
-        if not re.match("[^@]+@[^@]+\.[^@]+", email):
-            pass
-        return redirect("/")
+        error_msg = create_acc_validation(username, email, password, repeat_pw)
+        if ( error_msg != None):
+            db.new_user()
+        else:
+            return render_template("register.html", msg = error_msg)
     else:
-        msg = "Fill in all the required fields"
-        return render_template("register.html", msg = msg)
+        error_msg = "Fill in all the required fields"
+        return render_template("register.html", msg = error_msg)
+
+@app.route("/log_out")
+def log_out():
+    session.clear()
+    return redirect("/")
+
+def create_acc_validation(username, email, password, repeat_pw):
+    if password != repeat_pw:
+        return "Passwords do not match"
+    if len(username) < 6 or len(username) > 20:
+        return "Username must be between 6 and 20 characters"
+    if len(password) < 8 or len(password) > 20:
+        return "Password must be between 8 and 20 characters"
+    if not re.match("[^@]+@[^@]+\.[^@]+", email):
+        return "Invalid email address"
+
+    return None
