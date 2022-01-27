87d114a1149d5d87b107db88f16c2d9abd4cba19
oskrsjlnd <80696138+oskrsjlnd@users.noreply.github.com>
2022-01-27 17:18:07 +0200

    static_url_path and template_folder changed

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from repositories.database import db
 import os
 
 def create_app():
-    app = Flask(__name__, static_url_path="//home/g3xskgz/Repositories/sajinboard/app/static", template_folder="//home/g3xskgz/Repositories/sajinboard/app/templates")
+    app = Flask(__name__, static_url_path="./app/static", template_folder="./app/templates")
     app.config["SECRET_KEY"] = os.urandom(24).hex()
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///keijo"
     return app
