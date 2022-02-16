c3f6705a466b9aa29fa39c86a3a0ee04fc5f998f
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-02-16 16:33:58 +0200

    fix dbconn and queries

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 import os
-import getpass
 
-user = getpass.getuser()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24).hex()
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql:///{user}"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_ECHO'] = True
--- database.py
+++ database.py
@@ -8,37 +8,33 @@ class Repository:
         self.db = SQLAlchemy(app)
     
     def new_user(self, username, password, email):
-        pw_hash_value = generate_password_hash(password)
         try:
+            pw_hash_value = generate_password_hash(password)
             sql = """INSERT INTO users (username, password, email, admin)
                     VALUES (:username, :password, :email, :admin)"""
-            self.db.session.execute(sql, {"username":username, "password":pw_hash_value, "email":email, "admin":FALSE})
+            self.db.session.execute(sql, {"username":username, "password":pw_hash_value, "email":email, "admin":'f'})
             self.db.session.commit()
         except:
             return False
         return self.login(username, password)
     
     def login(self, username, password):
-        sql = """SELECT id, password, admin 
+        sql = """SELECT id, username, admin, password 
                 FROM users WHERE username=:username"""
         result = self.db.session.execute(sql, {"username":username})
         user = result.fetchone()
         if not user:
             return False
-        if check_password_hash(user.password, password):
-            session["user_id"] = user.id
-            session["username"] = user.username
-            session["admin"] = user.admin #voi olla ettei toimi vielä
+        if check_password_hash(user[3], password):
+            session["user_id"] = user[0]
+            session["username"] = user[1]
+            session["admin"] = user[2]
             return True
         return False
     
     def logout(self):
         del session["user_id"]
         del session["username"]
-        del session["admin"] #voi olla ettei tämäkään toimi
-
-    # def user_id(self):
-    #     return self.db.session.get("user_id", 0)
-
+        del session["admin"]
 
 db = Repository()
