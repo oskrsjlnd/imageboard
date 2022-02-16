8b17f92f4b1e414964038e1ea20ad7b3d03fc3f4
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-02-16 16:37:31 +0200

    create acc/login render correct templates

--- routes.py
+++ routes.py
@@ -5,39 +5,52 @@ from database import db
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", msg="")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    if username == "" or password == "":
-        return render_template("index.html", msg="Please give username and password")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == "" or password == "":
+            return render_template("index.html", msg="Please give username and password")
 
-    if db.login(username, password):
-        return redirect("/")
+        if db.login(username, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return render_template("index.html", msg="Invalid username or password")
     else:
-        return render_template("index.html", msg="Invalid username or password")
+        return render_template("index.html", msg="")
 
 @app.route("/create_acc", methods=["GET", "POST"])
 def create_acc():
-    username = request.form["username"]
-    email = request.form["email"]
-    password = request.form["password"]
-    repeat_pw = request.form["repeat_pw"]
-    if username == "" or email == "" or password == "" or repeat_pw == "":
-        return render_template("create_acc.html", msg = "Fill in all the required fields")
-        
-    if create_acc_validation(username, email, password, repeat_pw):
-        db.new_user(username, password, email)
-        return redirect("/")
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        repeat_pw = request.form["repeat_pw"]
+        if username == "" or email == "" or password == "" or repeat_pw == "":
+            return render_template("create_acc.html", msg="Fill in all the required fields")
+
+        if create_acc_validation(username, email, password, repeat_pw):
+            if db.new_user(username, password, email):
+                return redirect("/")
+            else:
+                return render_template("create_acc.html", msg="Username already exists")
+        else:
+            return render_template("create_acc.html", msg="Invalid username, password or email address")
     else:
-        return render_template("create_acc.html", msg = "Invalid username, password or email address")
+        return render_template("create_acc.html", msg="")
 
 @app.route("/log_out")
 def log_out():
     session.clear()
     return redirect("/")
+
+# @app.route("/my_uploads")
+# def my_uploads():
+#     return render_template("my_uploads.html")
 
 def create_acc_validation(username, email, password, repeat_pw):
     if (password != repeat_pw
