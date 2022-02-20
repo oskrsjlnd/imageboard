0d6bd8a68a22a1c281a229fc7fd4146befffb6e5
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-02-20 20:55:14 +0200

    separate app and db init

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask
-import os
+from os import urandom
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(24).hex()
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = urandom(24).hex()
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_ECHO'] = True
 
 import routes
