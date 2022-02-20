de3e5c4957bd4836238c77e27bf39d95ebd00c98
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-02-20 21:31:28 +0200

    secret key mod

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from os import urandom
+from os import getenv
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = urandom(24).hex()
+app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 app.config['DEBUG'] = True
 
 import routes
