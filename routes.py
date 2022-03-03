b14844b62732427bc2d80811f9d7bf28c7b8af90
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-03-04 00:56:49 +0200

    add routes for del user, my profile

--- routes.py
+++ routes.py
@@ -1,5 +1,4 @@
 from flask import render_template, request, redirect, session, abort
-import re
 from app import app
 import database
 
@@ -11,45 +10,39 @@ def index():
 def login():
     if request.method == "POST":
         username = request.form["username"]
-        password = request.form["password"]
-        if username == "" or password == "":
-            return render_template("index.html", msg="Please give username and password")
+        pwd = request.form["pwd"]
 
-        if database.login(username, password):
+        if database.login(username, pwd):
             return redirect("/")
         else:
             return render_template("index.html", msg="Invalid username or password")
     else:
         return render_template("index.html")
 
-@app.route("/user_list")
-def user_list():
-    if session["admin"]:
-        user_list = database.get_all_users()
-        return render_template("user_list.html", users=user_list)
-    else:
-        return render_template("notification.html", msg="You don't have authorization to view this page")
-
 @app.route("/create_acc", methods=["GET", "POST"])
 def create_acc():
+    msg = ""
     if request.method == "POST":
         username = request.form["username"]
         email = request.form["email"]
-        password = request.form["password"]
-        repeat_pw = request.form["repeat_pw"]
+        password = request.form["pwd"]
 
-        if username == "" or email == "" or password == "" or repeat_pw == "":
-            return render_template("create_acc.html", msg="Fill in all the required fields")
-
-        if create_acc_validation(username, email, password, repeat_pw):
-            if database.new_user(username, password, email):
-                return redirect("/")
-            else:
-                return render_template("create_acc.html", msg="Username or email already exists")
+        if database.new_user(username, password, email):
+            return redirect("/")
         else:
-            return render_template("create_acc.html", msg="Invalid username, password or email address")
+            msg="Username or email already taken"
+    
+    return render_template("create_acc.html", msg=msg)
+
+@app.route("/delete_user/<int:user_id>", methods=["POST"])
+def delete_user(user_id):
+    if "user_id" in session:
+        if user_id == session["user_id"] or session["admin"]:
+            database.delete_user_by_id(user_id)
+            log_out()
+            return redirect("/")
     else:
-        return render_template("create_acc.html", msg="")
+        return render_template("login_error.html")
 
 @app.route("/edit_admin_status/<int:user_id>", methods=["POST"])
 def edit_admin_status(user_id):
@@ -61,10 +54,32 @@ def edit_admin_status(user_id):
     else:
         return render_template("notification.html", msg="You are not authorized")
 
+@app.route("/user_list")
+def user_list():
+    if session["admin"]:
+        user_list = database.get_all_users()
+        return render_template("user_list.html", users=user_list)
+    else:
+        return render_template("notification.html", msg="You don't have authorization to view this page")
+
 @app.route("/log_out")
 def log_out():
     session.clear()
     return redirect("/")
+
+@app.route("/my_profile/<int:user_id>")
+def my_profile(user_id):
+    if "user_id" in session:
+        if session["user_id"] == user_id or session["admin"]:
+            user = database.get_user_by_id(user_id)
+            return render_template("my_profile.html", user=user)
+        else:
+            return render_template("notification.html", msg="You can only view your own user page")
+
+    else:
+        return render_template("login_error.html")            
+
+
 
 
 @app.route("/upload")
@@ -78,10 +93,7 @@ def search():
 @app.route("/category/<string:subject>/")
 def category(subject):
     subj_images = database.get_images_by_subject(subject)
-    if len(subj_images) > 0:
-        return render_template("category.html", data=subj_images, subject=subject)
-    else:
-        return render_template("category.html", subject=subject)
+    return render_template("category.html", data=subj_images, subject=subject)
 
 @app.route("/my_uploads")
 def my_uploads():
@@ -97,42 +109,38 @@ def random_image():
     image_data = database.get_random_image()
     comment_data = database.get_comments(image_data["image_id"])
     likes = database.get_likes(image_data["image_id"])
-
-    if image_data is not None:
-        return render_template("random_image.html", image_data=image_data, comment_data=comment_data, likes=likes)
-    else:
-        return render_template("random_image.html", msg="Whoops, 0 images on site")
+    msg = ""
+    if image_data is None:
+        msg = "Whoops, 0 images on site!"
+    return render_template("random_image.html", image_data=image_data, comment_data=comment_data, likes=likes, msg=msg)
 
 @app.route("/show_image/<int:image_id>")
 def show_image(image_id):
     image_data = database.get_image(image_id)
     comment_data = database.get_comments(image_id)
     likes = database.get_likes(image_id)
-
-    if image_data is not None:
-        return render_template("show_image.html", image_data=image_data, comment_data=comment_data, likes=likes)
-    else:
-        return render_template("show_image.html", msg="No image found")
-
+    msg = ""
+    if image_data is None:
+        msg = "No image found"
+    return render_template("show_image.html", image_data=image_data, comment_data=comment_data, likes=likes, msg=msg)
 
 @app.route("/send_image", methods=["POST"])
 def send_image():
     if session["csrf_token"] != request.form["csrf_token"]:
             return abort(403)
-    image = request.files["file"]
-    title = request.form["name"]
+    image = request.files["fileInput"]
+    title = request.form["title"]
     subject_name = request.form["subject"]
     user_id = session["user_id"]
     image_data = image.read()
-    image_file_name = image.filename
 
-    if image_upload_validation(image_file_name, title, subject_name, image_data):
-        if database.upload_image(user_id, subject_name, title, image_data):
-            return render_template("upload.html", msg="Image uploaded successfully")
-        else:
-            return render_template("upload.html", msg="Error while uploading image to database")
+    if len(image_data) > 2000*1024:
+        msg = "Maximum file size is 2MB"
+    elif database.upload_image(user_id, subject_name, title, image_data):
+        msg = "Image uploaded successfully"
     else:
-        return render_template("upload.html", msg="Check image requirements")
+        msg = "Error while uploading image to database"
+    return render_template("upload.html", msg=msg)
 
 @app.route("/delete_image/<int:user_id>/<int:image_id>", methods=["POST"])
 def delete_image(image_id, user_id):
@@ -141,10 +149,11 @@ def delete_image(image_id, user_id):
             return abort(403)
 
         if session["user_id"] == user_id:
-            if database.delete_image(image_id):
-                return render_template("notification.html", msg="Image successfully deleted")
+            database.delete_image(image_id)
+            msg = "Image successfully deleted"
         else:
-                return render_template("notification.html", msg="You can only delete your own images")
+            msg = "You can only delete your own images"
+        return render_template("notification.html", msg=msg)
     else:
         return render_template("login_error.html")
 
@@ -153,10 +162,10 @@ def edit_image_title(image_id, user_id):
     if "user_id" in session:
         if session["csrf_token"] != request.form["csrf_token"]:
             return abort(403)
-        if session["user_id"] == user_id:
+        if session["user_id"] == user_id or session["admin"]:
             new_title = request.form["new_title"]
-            if database.edit_image_title(image_id, new_title):
-                return redirect(f"/show_image/{image_id}")
+            database.edit_image_title(image_id, new_title)
+            return redirect(f"/show_image/{image_id}")
         else:
             return render_template("notification.html", msg="You can only edit titles of your images")
     else:
@@ -182,10 +191,8 @@ def post_comment(image_id):
             return abort(403)
         user_id = session["user_id"]
         content = request.form["comment"]
-        if database.post_comment(user_id, image_id, content):
-            return redirect(f"/show_image/{image_id}")
-        else:
-            return render_template("notification.html", msg="You can only delete your own posts")
+        database.post_comment(user_id, image_id, content)
+        return redirect(f"/show_image/{image_id}")
     else:
         return render_template("login_error.html")
 
@@ -194,25 +201,10 @@ def delete_comment(comment_id, image_id, user_id):
     if "user_id" in session:
         if session["csrf_token"] != request.form["csrf_token"]:
             return abort(403)
+
         if user_id == session["user_id"] or session["admin"]:
-            if database.delete_comment(comment_id):
-                return redirect(f"/show_image/{image_id}")
+            database.delete_comment(comment_id)
+            return redirect(f"/show_image/{image_id}")
 
-#helper functions
-def image_upload_validation(file_name, title, subject, image_data):
-    if (not file_name.endswith(".jpg")
-        or len(title) > 15
-        or len(subject) > 15
-        or len(image_data) > 2000*1024):
-        return False
-    return True
-
-def create_acc_validation(username, email, password, repeat_pw):
-    if (password != repeat_pw
-        or len(username) < 6 
-        or len(username) > 20
-        or len(password) < 8 
-        or len(password) > 20
-        or not re.match("[^@]+@[^@]+\.[^@]+", email)):
-        return False
-    return True
+        else:
+            return render_template("notification.html", msg="You can only delete your own posts")
